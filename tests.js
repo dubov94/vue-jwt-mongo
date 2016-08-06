6b805fe2053cb2b0085230514bec11150753ee1a
Pavel Dubov <dubov94@gmail.com>
2016-08-06 04:27:07 +0200

    Establish file structure

---
+++ tests.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const vueJwtMongo = require('./index.js')
+const chai = require('chai')
+
+describe('stuff', () => {
+    it('should launch', () => {
+        chai.assert(true)
+    })
+})
